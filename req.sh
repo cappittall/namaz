0cd63c81b0a23ff6e44a1b6ad04aa4a9fe0db2e9
Hakan Çetin <netcat16@gmail.com>
2023-10-23 10:11:44 +0300

    updated

--- req.sh
+++ req.sh
@@ -2,3 +2,6 @@
 sudo apt update
 sudo apt install -y libgirepository1.0-dev gcc libcairo2-dev pkg-config python3-dev gir1.2-gtk-3.0
 pip3 install wheel
+
+git clone https://gitlab.gnome.org/GNOME/pygobject.git
+sudo apt install meson ninja-build libgirepository1.0-dev gcc libcairo2-dev pkg-config python3-dev gir1.2-gtk-3.0
