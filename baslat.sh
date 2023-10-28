c646c709ce6de305ff764cec735e475efb83c5da
Hakan Çetin <netcat16@gmail.com>
2023-10-28 17:42:48 +0300

    updated

---
+++ baslat.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+# Activate Python environment
+source /var/Miniforge3/bin/activate namaz
+
+# Perform git pull or any other setup
+git pull
+
+# Run Python application
+# Attempt to run the first Python script
+python /home/mendel/namaz/app.py
+
+# Check if it was successful
+if [ $? -eq 0 ]; then
+  echo "First script executed successfully."
+else
+  echo "First script failed. Attempting second script."
+
+  # Attempt to run the second Python script
+  python ~/Documents/namaz/app.py
+
+  # Check if the second script was successful
+  if [ $? -eq 0 ]; then
+    echo "Second script executed successfully."
+  else
+    echo "Both scripts failed."
+  fi
+fi
