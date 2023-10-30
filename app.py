191ad677a7dae75cbf84a708d2b23cc7d398fd72
Hakan Çetin <netcat16@gmail.com>
2023-10-30 14:06:16 +0300

    updated

--- app.py
+++ app.py
@@ -71,15 +71,18 @@ def resize_image(image, target_height, target_width):
     return image
 
 def clear_directory(folder_path):
-    for filename in os.listdir(folder_path):
-        file_path = os.path.join(folder_path, filename)
-        try:
-            if os.path.isfile(file_path):
-                os.unlink(file_path)
-            elif os.path.isdir(file_path):
-                os.rmdir(file_path)
-        except Exception as e:
-            print(f"Failed to delete {file_path}. Reason: {e}")
+    try:
+        for filename in os.listdir(folder_path):
+            file_path = os.path.join(folder_path, filename)
+            try:
+                if os.path.isfile(file_path):
+                    os.unlink(file_path)
+                elif os.path.isdir(file_path):
+                    os.rmdir(file_path)
+            except Exception as e:
+                print(f"Failed to delete {file_path}. Reason: {e}")
+    except:
+        print(f"Failed to delete {file_path}. Reason: {e}")
             
 def resize_image_to_fixed(image, target_width, target_height):
     return cv2.resize(image, (target_width, target_height))
