321db27a42b18d3652e1257a1067ad8c409aa40e
Hakan Çetin <netcat16@gmail.com>
2023-11-12 15:39:32 +0300

    updated

--- tools/test2.py
+++ tools/test2.py
@@ -1,18 +1,23 @@
-
 import cv2
 import time
 
-cap = cv2.VideoCapture(1)
+# Try changing camera index to 0
+cap = cv2.VideoCapture(0)  
+
 while cap.isOpened():
     ret, im = cap.read()
     
-    cv2.putText(im, f'{time.time()}', (10,30), cv2.FONT_HERSHEY_COMPLEX, 1.0, (255,0,0), 1)
-    cv2.imshow('Baslik', im)
-    
+    if ret:
+        cv2.putText(im, f'{time.time()}', (10,30), cv2.FONT_HERSHEY_COMPLEX, 1.0, (255,0,0), 1)
+        cv2.imshow('Baslik', im)
+    else:
+        print("Failed to read frame")
+        break
+
     key = cv2.waitKey(1)
     print(key)
-    if key == 27:
+    if key == 27:  # ASCII for ESC
         break
-    
+
 cap.release()
 cv2.destroyAllWindows()
